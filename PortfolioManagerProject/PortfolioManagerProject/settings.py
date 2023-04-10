17a9701b71be662f2de50c3237ab14f1b9b526b0
minshallw12 <111906475+minshallw12@users.noreply.github.com>
2023-04-10 16:51:37 -0600

    PROJECT-002: Connected django, react, and postgresql

--- PortfolioManagerProject/PortfolioManagerProject/settings.py
+++ PortfolioManagerProject/PortfolioManagerProject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kj93tda61*=dfh9r$+-_419vkglduaz@t%-i1+%l^9v@ya3^x%'
+SECRET_KEY = os.environ["djangokey"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
